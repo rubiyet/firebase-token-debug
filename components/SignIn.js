e46c2b8f58a85ec01bd9414206f043eab10330a5
rubiyet <rubiyetfardous@gmail.com>
2022-08-21 14:10:21 +0600

    add token copy option

--- components/SignIn.js
+++ components/SignIn.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { app } from "../config/firebase";
 import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function SignIn() {
 
@@ -9,6 +10,10 @@ export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [token, setToken] = useState();
+
+  const tokenCopy = (e) => {
+    navigator.clipboard.writeText(token);
+  };
 
   const handleSignUp = async () => {
     try {
@@ -27,65 +32,77 @@ export default function SignIn() {
 
   return (
     <>
-    <div className="container">
-    <div className="row">
-    <div className="col">
-    </div>
-    <div className="col">
-      <div className="d-flex justify-content-center pt-3">
-        <div>
-          <div className="pb-1"><span>username: </span>
-            <input
-              type="email"
-              placeholder="Email"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              className="rounded border border-1"
-            />
+      <div className="container">
+        <div className="row">
+          <div className="col">
           </div>
-          <div className="pb-1"><span>Password: </span>
-            <input
-              type="Password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              className="rounded border border-1"
-            />
+          <div className="col">
+            <div className="d-flex justify-content-center pt-3">
+              <div>
+                <div className="pb-1"><span>username: </span>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    className="rounded border border-1"
+                  />
+                </div>
+                <div className="pb-1"><span>Password: </span>
+                  <input
+                    type="Password"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    className="rounded border border-1"
+                  />
+                </div>
+                <div className="d-flex justify-content-evenly pb-1">
+                  <button
+                    type="submit"
+                    onClick={handleSignUp}
+                    className="rounded border border-1"
+                  >
+                    Sign In
+                  </button>
+                  <button
+                    type="submit"
+                    onClick={handleClear}
+                    className="rounded border border-1"
+                  >
+                    Clear
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex justify-content-start pt-3">
+              <span>Token</span>            <button
+                type="submit"
+                onClick={tokenCopy}
+                className="border border-0 bg-white"
+              >
+                  <Image
+                    src="/copy.svg"
+                    alt="logo"
+                    width={15}
+                    height={15}
+                    className="rounded-md"
+                />
+              </button>
+            </div>
+            <div className="d-flex justify-content-center pt-1">
+              <div>
+                <textarea
+                  value={token}
+                  className={`${styles.tokenbox} rounded border border-1`}
+                  readOnly
+                />
+              </div>
+            </div>
           </div>
-          <div className="d-flex justify-content-evenly pb-1">
-            <button
-              type="submit"
-              onClick={handleSignUp}
-              className="rounded border border-1"
-            >
-              Sign In
-            </button>
-            <button
-              type="submit"
-              onClick={handleClear}
-              className="rounded border border-1"
-            >
-              Clear
-            </button>
+          <div className="col">
           </div>
         </div>
-      </div>
-      <div className="d-flex justify-content-start pt-3">
-        <span>Token: </span>
-      </div>
-      <div className="d-flex justify-content-center pt-1">
-        <div>
-          <textarea
-            value={token}
-            className={`${styles.tokenbox} rounded border border-1`}
-            readOnly
-          />
-        </div>
-      </div>
-      </div>
-      <div className="col">
-    </div>
-  </div>
       </div>
     </>
   )
