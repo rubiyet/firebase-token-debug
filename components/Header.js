d42e9020e1049c032f5fa49253b5ff7ce7a6fdde
Rubiyet Fardous <57152712+rubiyet@users.noreply.github.com>
2022-12-25 20:36:15 +0600

    remove ht logo

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,6 @@ export default function header() {
     return (
         <>
             <div  className={`${styles.navBar} container d-flex align-items-center justify-content-between p-4`}>
-                <Image src="/hellotask.svg" alt="" width={150} height={40} />
                 <div className="d-flex align-items-center"><Image src="/firebaseLogo.png" alt="" width={45} height={35} /><span className="text-white">Firebase Token Debug</span></div>
             </div>
         </>
