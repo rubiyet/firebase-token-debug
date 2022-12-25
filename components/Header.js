e2a2cc93085ffca4aca0aacaa7987f4ca8afb12b
Rubiyet Fardous <57152712+rubiyet@users.noreply.github.com>
2022-12-25 20:38:59 +0600

    logo width changed

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function header() {
     return (
         <>
             <div  className={`${styles.navBar} container d-flex align-items-center justify-content-between p-4`}>
-                <div className="d-flex align-items-center"><Image src="/firebaseLogo.png" alt="" width={45} height={35} /><span className="text-white">Firebase Token Debug</span></div>
+                <div className="d-flex align-items-center"><Image src="/firebaseLogo.png" alt="" width={40} height={35} /><span className="text-white">Firebase Token Debug</span></div>
             </div>
         </>
     )
